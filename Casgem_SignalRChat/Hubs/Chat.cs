bc8423871d4b666a5759b72d42da129f04bdc3da
Burak <99614754+burakkertn@users.noreply.github.com>
2023-07-30 13:45:05 +0300

    Add project files.

---
+++ Casgem_SignalRChat/Hubs/Chat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Casgem_SignalRChat.Hubs
+{
+    public class Chat:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
